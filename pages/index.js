c573f62548dda8b38382a03a6e2e2ab786af996f
t0931791533 <134581657+t0931791533@users.noreply.github.com>
2023-06-15 14:33:52 +0800

    index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="歡迎來到我首頁              " />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
